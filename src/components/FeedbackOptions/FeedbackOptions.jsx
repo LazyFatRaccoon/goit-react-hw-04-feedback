7e4ce67a5db4ab9add0b7da9ed522aa9ee3e31dc
Vladimir Todorov <vladimir.todorov.ukraine@gmail.com>
2022-09-15 21:18:05 +0300

    a

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,38 +1,36 @@
 import { Button, Div } from './FeedbackOptions.styled';
 import PropTypes from 'prop-types';
 
-const FeedbackOptions = ({onLeaveFeedback }) => {
+const FeedbackOptions = ({ onLeaveFeedback }) => {
   return (
     <Div>
-      
-          <Button
-          name="good"
-            type="button"
-            onClick={(e) => {
-              onLeaveFeedback(e);
-            }}
-          >
-            good
-          </Button>
-          <Button
-            name="neutral"
-            type="button"
-            onClick={(e) => {
-              onLeaveFeedback(e);
-            }}
-          >
-            neutral
-          </Button>
-          <Button
-          name="bad"
-            type="button"
-            onClick={(e) => {
-              onLeaveFeedback(e);
-            }}
-          >
-            bad
-          </Button>
-       
+      <Button
+        name="good"
+        type="button"
+        onClick={e => {
+          onLeaveFeedback(e);
+        }}
+      >
+        good
+      </Button>
+      <Button
+        name="neutral"
+        type="button"
+        onClick={e => {
+          onLeaveFeedback(e);
+        }}
+      >
+        neutral
+      </Button>
+      <Button
+        name="bad"
+        type="button"
+        onClick={e => {
+          onLeaveFeedback(e);
+        }}
+      >
+        bad
+      </Button>
     </Div>
   );
 };
